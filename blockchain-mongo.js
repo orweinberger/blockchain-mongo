52f8bb08e5f072020a195ab300ea6d5febf443c6
orweinberger <orweinberger@gmail.com>
2013-11-06 10:52:33 +0200

    Added a resume option

--- blockchain-mongo.js
+++ blockchain-mongo.js
@@ -3,14 +3,21 @@ var mongo = require('./lib/mongo');
 
 var options = {
   speed: 2000, // Run every X ms
-  tryResume: true // Settings this to true will fetch the last block height and resume the db insertion
+  tryResume: true // Settings this to true will fetch the last block index and resume the db insertion
 }
 bc = new blockchain();
 
 var blockindex = 1;
+
+if (options.tryResume) {
+  mongo.fetch('blocks', {}, {"sort": {"block_index": -1}, "limit": 1}, function (err, res) {
+    blockindex = res[0].block_index + 1;
+  });
+}
+
 setInterval(function () {
   bc.API('block-index', blockindex, function (res) {
-    mongo.insert('block-height', res, function () {
+    mongo.insert('blocks', res, function () {
       console.log('Inserted block index: ' + blockindex);
       blockindex++;
     });
